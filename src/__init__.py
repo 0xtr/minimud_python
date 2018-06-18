99db3d7fd03016d5ba5ea5a54dcfa3fa5d83aa15
Tara Null <tn@fastmail.net>
2018-06-18 20:43:30 +0100

    Break the world

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from . import util_custom
+from . import sqlite_custom
--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 import random
 import socket
 import sys
-import sqlite
+import sqlite3
 import utils
 
-from sqlite_util import *
-from util import *
+from . import sqlite_custom
+from . import util_custom
 
 # get a suitable port
 port = random.randint(5000, 6000)
-print "Use port %d for connections\n", port
+print("Use port %d for connections\n", port)
 
 # open the sqlite3 dbs
-assert sqlite_util.init_dbs() == sys.EXIT_SUCCESS
+assert sqlite_custom.init_dbs() == 0
 
 # create the master socket
 listen_socket = socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,14 +21,14 @@ listen_socket.setblocking(0)
 # bind it to our chosen port
 try: 
     socket.bind(listen_socket, '')
-except Exception e:
-    print e.args
-    sys.exit(sys.EXIT_FAILURE)
+except Exception as e:
+    print(e.args)
+    sys.exit(1)
 
 # set listener for connections
 if socket.listen(listen_socket, SOMAXCONN) != 0:
-    print "Listening for connections failed"
-    sys.exit(sys.EXIT_FAILURE)
+    print("Listening for connections failed")
+    sys.exit(1)
 
 iterate = True
 inputs = [listen_socket]
@@ -37,5 +37,5 @@ outputs = []
 while (iterate):
     readable, writable, exceptional = select.select(inputs, outputs, inputs)
 
-print "minimud server exited"
-sys.exit(sys.EXIT_FAILURE)
+print("minimud server exited")
+sys.exit(1)
